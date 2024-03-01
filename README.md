793c2b99a4f8710b3e6fbb1e6d21008d177f8a79
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-01 11:47:36 -0600

    progress

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ Here are the steps I took to create this project.
 - `python -m venv .venv`
 - `. .venv/bin/activate`
 - `pip install fastapi`
-- `pip install jinja2`
-- `pip install uvicorn`
+- `pip install jinja2` for HTML templates
+- `pip install python-multipart` for form data
+- `pip install uvicorn` for local server
 - Create all the files in this repo.
 - Start the server by entering `./run`
 - Browse localhost:5000
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import Annotated
 import uuid
 
 app = FastAPI()
@@ -36,24 +37,21 @@ def one_dog(id):
     dog = dog_map[id]
     return dog # returns JSON
 
-# # Deletes the dog with a given id.
-# @app.route('/dog/<id>', methods=['DELETE'])
-# def delete_dog(id):
-#     global dog_map
-#     del dog_map[id]
-#     return ''
+# Deletes the dog with a given id.
+@app.delete('/dog/{id}', response_class=HTMLResponse)
+def delete_dog(id):
+    global dog_map
+    del dog_map[id]
 
-# # Deselects the currently selected dog.
-# @app.route('/deselect')
-# def deselect():
-#     global selected_id
-#     selected_id = ''
-#     res = Response('')
-#     res.headers['HX-Trigger'] = 'selection-change'
-#     return res
+# Deselects the currently selected dog.
+@app.get('/deselect')
+def deselect(response: Response):
+    global selected_id
+    selected_id = ''
+    response.headers['HX-Trigger'] = 'selection-change'
 
 # Gets the proper form for either adding or updating a dog.
-@app.get('/form')
+@app.get('/form', response_class=HTMLResponse)
 def form(request: Request):
     dog = dog_map.get(selected_id)
     return templates.TemplateResponse(
@@ -61,45 +59,51 @@ def form(request: Request):
     )
 
 # Gets table rows for all the dogs.
-@app.get('/rows')
+@app.get('/rows', response_class=HTMLResponse)
 def rows(request: Request):
     sorted_dogs = sorted(dog_map.values(), key=lambda x: x['name'])
     return templates.TemplateResponse(
         request=request, name="dog-rows.html", context={"dogs": sorted_dogs}
     )
 
-# # Selects a dog.
-# @app.route('/select/<id>')
-# def select(id):
-#     global selected_id
-#     selected_id = id;
-#     res = Response('')
-#     res.headers['HX-Trigger'] = 'selection-change'
-#     return res
+# Selects a dog.
+@app.get('/select/{id}')
+def select(response: Response, id):
+    global selected_id
+    selected_id = id;
+    response.headers['HX-Trigger'] = 'selection-change'
 
-# # Creates a dog.
-# @app.route('/dog', methods=['POST'])
-# def create():
-#     name = request.form.get('name')
-#     breed = request.form.get('breed')
-#     new_dog = add_dog(name, breed);
-#     return render_template('dog-row.html', dog=new_dog, status=201);
+# Creates a dog.
+@app.post('/dog', response_class=HTMLResponse)
+def create(
+    request: Request,
+    name: Annotated[str, Form()],
+    breed: Annotated[str, Form()]
+):
+    new_dog = add_dog(name, breed);
+    return templates.TemplateResponse(
+        request=request, status=201, name="dog-row.html", context={"dog": new_dog}
+    )
 
-# # Updates a dog
-# @app.route('/dog/<id>', methods=['PUT'])
-# def update(id):
-#     name = request.form.get('name')
-#     breed = request.form.get('breed')
-#     updatedDog = {'id': id, 'name': name, 'breed': breed};
+# Updates a dog
+@app.put('/dog/{id}', response_class=HTMLResponse)
+def update(
+    request: Request,
+    response: Response,
+    name: Annotated[str, Form()],
+    breed: Annotated[str, Form()],
+    id
+):
+    updatedDog = {'id': id, 'name': name, 'breed': breed};
 
-#     global dog_map
-#     dog_map[id] = updatedDog;
+    global dog_map
+    dog_map[id] = updatedDog;
 
-#     global selected_id
-#     selected_id = '';
+    global selected_id
+    selected_id = '';
 
-#     res = make_response(
-#         render_template('dog-row.html', dog=updatedDog, swap=True)
-#     )
-#     res.headers['HX-Trigger'] = 'selection-change'
-#     return res
+    response.headers['HX-Trigger'] = 'selection-change'
+    return templates.TemplateResponse(
+        request=request, name="dog-row.html", context={"dog": updatedDog, "swap": True}
+    )
+    return res
